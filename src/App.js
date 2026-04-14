4a2b9e08913c6ee66dd6d9c9262f2aabc6fc9521
Davi Pedrosa <davipedrosassss@gmail.com>
2026-04-14 16:35:19 -0300

    Inserindo a pastas components em nosso projeto, que como o próprio nome diz, conterá os componentes do mesmo, que utilizaremos para produzir nossas paginas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Main from './components/Main';
 import './App.css';
+
 
 function App(){
   return(
     <div>
       <h1>Meu primeiro app em React</h1>
+      <Main />
 
       <p>Bem-vindo ao meu primeiro app em React!</p>
       <p>Este é um exemplo simples para mostrar como criar um componente React e renderizá-lo na tela.</p>
