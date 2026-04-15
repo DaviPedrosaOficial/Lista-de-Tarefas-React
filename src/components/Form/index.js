19c112da494626e871e25fc5c3cf24f712c41f5f
Davi Pedrosa <davipedrosassss@gmail.com>
2026-04-15 17:50:41 -0300

    Foi instalado o pacote prop-types ao projeto, para auxiliar a verificar se uma propriedade de um componente é de fato uma prop, e nos auxiliar na desestruturação dos elementos de nosso components, como foi feito em nosso Form e Ul

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,13 +4,15 @@ import './Form.css';
 
 import { FaPlus } from 'react-icons/fa'; // Importando o botão de adicionar tarefa
 
-export default function Form (props) {
+import PropTypes from 'prop-types'; // Importando a biblioteca PropTypes para validar as props (propriedades) do componente Form
+
+export default function Form ({handleSubmit, handleChange, novaTarefa}) {
   return (
-      <form onSubmit={props.handleSubmit} className='form'>
+      <form onSubmit={handleSubmit} className='form'>
 
         <input
-          onChange={props.handleChange}
-          value={props.novaTarefa}
+          onChange={handleChange}
+          value={novaTarefa}
           type="text"
           placeholder="Digite uma tarefa" />
 
@@ -20,4 +22,10 @@ export default function Form (props) {
 
       </form>
   );
+}
+
+Form.propTypes = {
+  handleSubmit : PropTypes.func.isRequired, // Validando se a funcão handleSubmit é passada como prop e é obrigatória para o componente Form
+  handleChange : PropTypes.func.isRequired, // Validando se a função handleChange é passada como prop e é obrigatória para o componente Form
+  novaTarefa : PropTypes.string.isRequired, // Validando se a String novaTarefa é uma propriedade e obrigatória para o componente Form
 }
--- src/components/Ul/index.js
+++ src/components/Ul/index.js
@@ -4,21 +4,23 @@ import './Ul.css';
 
 import { FaEdit, FaTrash } from 'react-icons/fa'; // Importando os botões de editar e excluir para as tarefas
 
-export default function Ul (props){
+import PropTypes from 'prop-types'; // Importando a biblioteca PropTypes para validar as props (propriedades) do componente Ul
+
+export default function Ul ({tarefas, handleEdit, handleDelete}) {
   return (
     <ul className='tarefas'>
-      {props.tarefas.map((tarefa, index) =>
+      {tarefas.map((tarefa, index) =>
         <li key={index}>
           {tarefa}
           <div>
             <button
-              onClick={() => props.handleEdit(index)}
+              onClick={() => handleEdit(index)}
               type="button"
               className='edit'>
               <FaEdit />
             </button>
             <button
-              onClick={() => props.handleDelete(index)}
+              onClick={() => handleDelete(index)}
               type="button"
               className='delete'>
               <FaTrash />
@@ -28,4 +30,10 @@ export default function Ul (props){
         </li>)}
     </ul>
   )
+}
+
+Ul.propTypes = {
+  tarefas: PropTypes.arrayOf(PropTypes.string).isRequired, // Validando se a prop tarefas é um array de strings e é obrigatória para o componente Ul
+  handleEdit: PropTypes.func.isRequired, // Validando se a função handleEdit é passada como prop e é obrigatória para o componente Ul
+  handleDelete: PropTypes.func.isRequired, // Validando se a função handleDelete é passada como prop e é obrigatória para o componente Ul
 }
