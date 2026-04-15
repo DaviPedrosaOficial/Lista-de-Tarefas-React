afc7e7bbda97dfb976fa0148c05c80fd3c0a3a97
Davi Pedrosa <davipedrosassss@gmail.com>
2026-04-14 21:06:08 -0300

    Entregando a o css de nosso main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
+// Importando o CSS do componente para o Formulario
+import { FaPlus } from 'react-icons/fa';
+
+// Importando os botões de editar e excluir para as tarefas
+import { FaEdit, FaTrash } from 'react-icons/fa';
+
+import './Main.css';
+
 export default class Main extends Component {
   state = {
     novaTarefa: '',
     tarefas: [],
   };
 
-  inputTarefa = (eventoOnChange) => {
+  handleChange = (eventoOnChange) => {
     this.setState({
       novaTarefa: eventoOnChange.target.value,
     })
@@ -22,18 +30,43 @@ export default class Main extends Component {
   }
 
   render() {
+
+    const { novaTarefa, tarefas } = this.state;
+
     return (
+
       <div className='main'>
+
         <h1>Lista de Tarefas</h1>
 
-        <form onSubmit={this.adicionaTarefa}>
-          <input onChange={this.inputTarefa} value={this.state.novaTarefa} type="text" placeholder="Digite uma tarefa" />
-          <button type="submit">Adicionar</button>
+        <form onSubmit={this.adicionaTarefa} className='form'>
+
+          <input
+            onChange={this.handleChange}
+            value={novaTarefa}
+            type="text"
+            placeholder="Digite uma tarefa" />
+
+          <button type="submit">
+            <FaPlus />
+          </button>
+
         </form>
 
         <ul className='tarefas'>
-          {this.state.tarefas.map((tarefas, index) =>
-            <li key={index}>{tarefas}</li>)}
+          {tarefas.map((tarefa, index) =>
+            <li key={index}>
+              {tarefa}
+              <div>
+                <button type="button" className='edit'>
+                  <FaEdit />
+                </button>
+                <button type="button" className='delete'>
+                  <FaTrash />
+                </button>
+              </div>
+
+            </li>)}
         </ul>
       </div>
     );
